7a7e54c494940bff1be8c72590bda7e563b4d2fb
Maria Eduarda <dudaamadorm@gmail.com>
2025-03-16 14:02:58 -0300

    Add files via upload

---
+++ TiposVariaveis.java
@@ -0,0 +1,11 @@
+package exercicios;
+
+public class TiposVariaveis {
+	public static void main(String[] args) {
+		String nome = "Maria";
+		int idade = 18;
+		
+		System.out.printf("A usuária %s tem %d anos", nome,idade);
+	}
+
+}
